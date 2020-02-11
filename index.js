f4cae011e25c9c04f31679427309147033d3e136
Reinoptland <reinoptland@gmail.com>
2020-02-11 02:05:19 +0100

    Get input, assign to variables and test

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function bmiCalculator() {
-  console.log("testing main function");
+  const weightInKg = parseInt(process.argv[2]);
+  const heightInM = parseFloat(process.argv[3]);
+  const age = parseInt(process.argv[4]);
+  const dailyExercise = process.argv[5];
+  const gender = process.argv[6];
+
+  console.log("WEIGHT: ", weightInKg);
+  console.log("HEIGHT: ", heightInM);
+  console.log("AGE: ", age);
+  console.log("DAILY EXERCISE: ", dailyExercise);
+  console.log("GENDER: ", gender);
 }
 
 bmiCalculator();
