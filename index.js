4fec8a9c0771e3f21171c415831403e29d58f6e7
Reinoptland <reinoptland@gmail.com>
2020-02-11 02:26:44 +0100

    Define calculateBMI, call to test

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function calculateBMI() {
+  console.log("called?");
+}
+
 function bmiCalculator() {
   const weightInKg = parseInt(process.argv[2]);
   const heightInM = parseFloat(process.argv[3]);
@@ -5,11 +9,14 @@ function bmiCalculator() {
   const dailyExercise = process.argv[5];
   const gender = process.argv[6];
 
-  console.log("WEIGHT: ", weightInKg);
-  console.log("HEIGHT: ", heightInM);
-  console.log("AGE: ", age);
-  console.log("DAILY EXERCISE: ", dailyExercise);
-  console.log("GENDER: ", gender);
+  calculateBMI();
+
+  //   Comment out input checks for now
+  //   console.log("WEIGHT: ", weightInKg);
+  //   console.log("HEIGHT: ", heightInM);
+  //   console.log("AGE: ", age);
+  //   console.log("DAILY EXERCISE: ", dailyExercise);
+  //   console.log("GENDER: ", gender);
 }
 
 bmiCalculator();
